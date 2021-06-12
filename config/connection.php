9fdd6aca885e99804a56c9d261766c1b939d579e
HimanshuBohra <39338459+HimanshuBohra@users.noreply.github.com>
2021-06-12 17:28:48 +0530

    Update connection.php

--- config/connection.php
+++ config/connection.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$database = "breathe";
+$servername = "remotemysql.com";
+$username = "ML7FyYXtyJ";
+$password = "0YTiiv12RO";
+$database = "ML7FyYXtyJ";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $database);
